e609440f7396bdbbe29853cb151d600f977f634e
dschmidt <dschmidt@paloaltonetworks.com>
2023-11-08 10:53:13 -0500

    enabling versioning

--- tf/illuminate_user_1.tf
+++ tf/illuminate_user_1.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "illuminate_user_1" {
   bucket        = "illuminate-chicago-bucket-for-illuminate-user-1"
 
+  versioning {
+      enabled = true
+  }
+
   tags = {
     Owner        = "illuminate_user_1"
     Environment = "Illuminate Chicago Workshop 12072023"
