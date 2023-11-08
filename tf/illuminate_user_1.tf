9abac26d939c073b5b2f9898a4dd803c91175c94
dschmidt <dschmidt@paloaltonetworks.com>
2023-11-08 10:40:32 -0500

    resetting

--- tf/illuminate_user_1.tf
+++ tf/illuminate_user_1.tf
@@ -1,12 +1,1 @@
-resource "aws_s3_bucket" "illuminate_user_1" {
-  bucket        = "illuminate-chicago-bucket-for-illuminate-user-1"
-
-  tags = {
-    Owner        = "illuminate_user_1"
-    Environment = "Illuminate Chicago Workshop 12072023"
-    SomeTag     = "somevalue"
-  }
-
-  force_destroy = true
-}
 
