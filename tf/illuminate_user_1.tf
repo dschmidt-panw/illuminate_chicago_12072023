a5e5e7ebade1c96545af0ffa0aa74ee5be9ad4eb
dschmidt <dschmidt@paloaltonetworks.com>
2023-11-08 10:29:17 -0500

    adding public acl

--- tf/illuminate_user_1.tf
+++ tf/illuminate_user_1.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "illuminate_user_1" {
   bucket        = "illuminate-chicago-bucket-for-illuminate-user-1"
 
+  acl    = "public-read-write"
+  
   tags = {
     Owner        = "illuminate_user_1"
     Environment = "Illuminate Chicago Workshop 12072023"
