b024b1b43fb97bed2ba73193398c64d456f583f9
deschmid <dennis.e.schmidt@gmail.com>
2023-11-08 09:42:20 -0500

    Update illuminate_user_1.tf

--- tf/illuminate_user_1.tf
+++ tf/illuminate_user_1.tf
@@ -6,5 +6,13 @@ resource "aws_s3_bucket" "illuminate_user_1" {
     Environment = "Illuminate Chicago Workshop 12072023"
   }
 
+resource "aws_s3_bucket_versioning" "illuminate_bucket_1_versioning" {
+  bucket = aws_s3_bucket.illuminate_user_1.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
   force_destroy = true
 }
