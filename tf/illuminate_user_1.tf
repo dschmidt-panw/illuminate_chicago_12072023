e68e1b4547c7c3540f430ffa83f79c5b8a7c87ea
dschmidt <dschmidt@paloaltonetworks.com>
2023-11-08 15:37:16 -0500

    fixing

--- tf/illuminate_user_1.tf
+++ tf/illuminate_user_1.tf
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_versioning" "s3_versioning_for_illuminate_user_1" {
 }
 
 resource "aws_s3_bucket_public_access_block" "illuminate_bucket_1_access_policy" {
-  bucket = aws_s3_bucket.illuminate_bucket_1.id
+  bucket = aws_s3_bucket.illuminate_user_1.id
   # replace 'false' with 'true' below
   block_public_acls   = false
   block_public_policy = false
