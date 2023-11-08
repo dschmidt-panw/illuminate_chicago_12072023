bceaf2e277c2fc77ee91564b3217de9ad90745ac
dschmidt <dschmidt@paloaltonetworks.com>
2023-11-08 15:44:54 -0500

    recreating resource

---
+++ tf/illuminate_user_1.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "illuminate_user_1" {
+  bucket        = "illuminate-chicago-bucket-for-illuminate-user-1"
+
+  tags = {
+    Owner        = "illuminate_user_1"
+    Environment = "Illuminate Chicago Workshop 12072023"
+    Replace_this_tag  = "Hello Illuminate Workshop"
+  }
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "s3_versioning_for_illuminate_user_1" {
+  bucket = aws_s3_bucket.illuminate_user_1.id
+  versioning_configuration {
+    status = "Suspended"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "illuminate_bucket_1_access_policy" {
+  bucket = aws_s3_bucket.illuminate_user_1.id
+  block_public_acls   = false
+  block_public_policy = false
+  restrict_public_buckets = false
+  ignore_public_acls = false
+}
