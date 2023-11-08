1fadb440b11564bf7461a577fd612681c20c2ae0
dschmidt <dschmidt@paloaltonetworks.com>
2023-11-08 10:08:59 -0500

    removed acl

--- tf/illuminate_access_logging_bucket.tf
+++ tf/illuminate_access_logging_bucket.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "illuminate_log_bucket" {
   bucket = "illuminate-chicago-12072023-log-bucket"
 }
 
-resource "aws_s3_bucket_acl" "illuminate_log_bucket_acl" {
-  bucket = aws_s3_bucket.illuminate_log_bucket.id
-  acl    = "log-delivery-write"
-}
+#resource "aws_s3_bucket_acl" "illuminate_log_bucket_acl" {
+#  bucket = aws_s3_bucket.illuminate_log_bucket.id
+#  acl    = "log-delivery-write"
+#}
