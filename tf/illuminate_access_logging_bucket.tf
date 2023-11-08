77f8c6a3c21c2beec620b83372f8acb67cecfac8
dschmidt <dschmidt@paloaltonetworks.com>
2023-11-08 10:06:43 -0500

    test

---
+++ tf/illuminate_access_logging_bucket.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "illuminate_log_bucket" {
+  bucket = "illuminate-chicago-12072023-log-bucket"
+}
+
+resource "aws_s3_bucket_acl" "illuminate_log_bucket_acl" {
+  bucket = aws_s3_bucket.illuminate_log_bucket.id
+  acl    = "log-delivery-write"
+}
