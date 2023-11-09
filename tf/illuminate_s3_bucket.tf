71f656b7b4cad76c6976016f374bf7d03f2e501d
deschmid <dennis.e.schmidt@gmail.com>
2023-11-09 15:42:50 -0500

    Update illuminate_s3_bucket.tf

--- tf/illuminate_s3_bucket.tf
+++ tf/illuminate_s3_bucket.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "illuminate_user_1" {
+resource "aws_s3_bucket" "illuminate_bucket" {
   bucket        = "illuminate-chicago-12092023-bucket"
 
   tags = {
@@ -9,15 +9,15 @@ resource "aws_s3_bucket" "illuminate_user_1" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_versioning" "s3_versioning_for_illuminate_user_1" {
-  bucket = aws_s3_bucket.illuminate_user_1.id
+resource "aws_s3_bucket_versioning" "s3_versioning_for_illuminate_bucket" {
+  bucket = aws_s3_bucket.illuminate_bucket.id
   versioning_configuration {
     status = "Suspended"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "illuminate_user_1_bucket_access_policy" {
-  bucket = aws_s3_bucket.illuminate_user_1.id
+resource "aws_s3_bucket_public_access_block" "illuminate_bucket_access_policy" {
+  bucket = aws_s3_bucket.illuminate_bucket.id
   block_public_acls   = false
   block_public_policy = false
   restrict_public_buckets = false
