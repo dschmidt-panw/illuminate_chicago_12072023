363bfe71e6a9c84c4cc21a8d62d0a755e30e2b70
deschmid <dennis.e.schmidt@gmail.com>
2023-11-13 11:34:15 -0500

    Update illuminate_s3_bucket.tf

--- tf/illuminate_s3_bucket.tf
+++ tf/illuminate_s3_bucket.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "illuminate_bucket" {
   bucket        = "illuminate-chicago-12092023-bucket"
 
   tags = {
-    Owner       = "Palo Alto Networks - Prisma Cloud"
+    Replace_this_tag       = "Palo Alto Networks - Prisma Cloud"
     Environment = "Illuminate Chicago Workshop 12072023"
   }
 
@@ -12,14 +12,14 @@ resource "aws_s3_bucket" "illuminate_bucket" {
 resource "aws_s3_bucket_versioning" "s3_versioning_for_illuminate_bucket" {
   bucket = aws_s3_bucket.illuminate_bucket.id
   versioning_configuration {
-    status = "Enabled"
+    status = "Suspended"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "illuminate_bucket_access_policy" {
   bucket = aws_s3_bucket.illuminate_bucket.id
-  block_public_acls   = true
-  block_public_policy = true
-  restrict_public_buckets = true
-  ignore_public_acls = true
+  block_public_acls   = false
+  block_public_policy = false
+  restrict_public_buckets = false
+  ignore_public_acls = false
 }
